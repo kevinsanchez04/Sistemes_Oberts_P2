cb5ccaaa3cc4047e48a86d90dbae5c4ff39c1b1c
Iulian Sebastian Oprea <iuliansebastian.oprea@estudiants.urv.cat>
2024-12-15 17:55:05 +0100

    Filter Controller ADD

--- Homework2/src/main/java/deim/urv/cat/homework2/controller/ArticleController.java
+++ Homework2/src/main/java/deim/urv/cat/homework2/controller/ArticleController.java
@@ -4,6 +4,9 @@ import deim.urv.cat.homework2.service.ArticleService;
 import jakarta.inject.Inject;
 import jakarta.mvc.Controller;
 import jakarta.mvc.Models;
+import jakarta.mvc.UriRef;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.BeanParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -16,8 +19,8 @@ public class ArticleController {
     @Inject Models models;
     
     @GET
-    public String showForm() {
-        models.put("articles",service.findAll());
+    public String showAll() {
+        models.put("articles",service.findAll(null));
         return "articles.jsp"; // Injects CRSF token
     }
     
@@ -27,4 +30,13 @@ public class ArticleController {
         models.put("article",service.findId(id));
         return "article.jsp";
     }
+    
+    @GET
+    @Path("/filter")
+    @UriRef("/filter")
+    public String filter(@Valid @BeanParam FilterForm filter) {
+        models.put("articles",service.findAll(filter));
+        return "articles.jsp"; // Injects CRSF token
+    }
+    
 }
