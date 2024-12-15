1f5ca3c0b9f7026cd70cbb816da9948af367b992
Iulian Sebastian Oprea <iuliansebastian.oprea@estudiants.urv.cat>
2024-12-15 17:56:11 +0100

    Added FilterForm

--- Homework2/src/main/java/deim/urv/cat/homework2/service/ArticleService.java
+++ Homework2/src/main/java/deim/urv/cat/homework2/service/ArticleService.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 import deim.urv.cat.homework2.model.ArticleDTO;
 import deim.urv.cat.homework2.model.ArticleGQ;
+import deim.urv.cat.homework2.controller.FilterForm;
 
 public interface ArticleService {
-    List<ArticleGQ> findAll();
+    List<ArticleGQ> findAll(FilterForm filter);
     ArticleDTO findId(Long id);
 }
