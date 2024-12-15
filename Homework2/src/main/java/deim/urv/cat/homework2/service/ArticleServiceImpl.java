3f68e5a401c5523fc134f398c20b8e4609d30371
Iulian Sebastian Oprea <iuliansebastian.oprea@estudiants.urv.cat>
2024-12-15 17:56:34 +0100

    New Version With FilterForm

--- Homework2/src/main/java/deim/urv/cat/homework2/service/ArticleServiceImpl.java
+++ Homework2/src/main/java/deim/urv/cat/homework2/service/ArticleServiceImpl.java
@@ -1,67 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package deim.urv.cat.homework2.service;
 
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 import deim.urv.cat.homework2.model.ArticleDTO;
 import deim.urv.cat.homework2.model.ArticleGQ;
 import deim.urv.cat.homework2.model.Topic;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.GenericType;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import deim.urv.cat.homework2.controller.FilterForm;
+import deim.urv.cat.homework2.controller.UserForm;
+import jakarta.ws.rs.client.Entity;
+import java.util.stream.Collectors;
 
 public class ArticleServiceImpl implements ArticleService {
     
+    private final WebTarget webTarget;
+    private final jakarta.ws.rs.client.Client client;
+    private static final String BASE_URI = "http://localhost:8080/Homework1/rest/api/v1";
+    
+    public ArticleServiceImpl() {
+        client = jakarta.ws.rs.client.ClientBuilder.newClient();
+        webTarget = client.target(BASE_URI).path("article");
+    }
+    
     @Override
-    public List<ArticleGQ> findAll(){
-        
-        List<ArticleGQ> lista = new LinkedList<>();
-        
-        // Artículo 1
-        ArticleGQ article1 = new ArticleGQ();
-        article1.setTitol("Título del artículo 4");
-        article1.setResum("Resum del article 4");
-        article1.setVisualitzacions(4500);
-        article1.setPrivacitat(true);
-        article1.setData("2024-11-14");
-        article1.setImatge("https://www.caracteristicass.de/wp-content/uploads/2023/02/imagenes-artisticas.jpg");
-        article1.setAutor("Autor4");
-        lista.add(article1);
-        // Artículo 2
-        ArticleGQ article2 = new ArticleGQ();
-        article2.setTitol("Título del artículo 5");
-        article2.setResum("Resum del article 5");
-        article2.setPrivacitat(false);
-        article2.setVisualitzacions(4400);
-        article2.setData("2024-11-15");
-        article2.setImatge("https://www.caracteristicass.de/wp-content/uploads/2023/02/imagenes-artisticas.jpg");
-        article2.setAutor("Autor5");
-        
-        for(int i = 0; i< 5;i++){
-            lista.add(article2);  
-        }       
-        return lista;
+    public List<ArticleGQ> findAll(FilterForm filter){
+        WebTarget target = webTarget;
+        if (filter != null){
+            for(Topic topic : filter.getTopics()){
+                target = target.queryParam("topic", topic.name());
+            }
+            if(!filter.getAuthor().equals("NULL")){
+                target = target.queryParam("author", filter.getAuthor());
+            }
+        }
+        Response response = target.request(MediaType.APPLICATION_JSON).get();
+        if (response.getStatus() == 200) {
+            return response.readEntity(new GenericType<List<ArticleGQ>>() {});
+        }
+        return null;
     };
     
     @Override
     public ArticleDTO findId(Long id){
-        ArticleDTO article1 = new ArticleDTO();
-        article1.setTitol("Título del artículo 4");
-        article1.setText("The night sky is a mesmerizing tapestry of stars, planets, and celestial wonders. "+
-                    "The Milky Way, a galaxy containing our Solar System, stretches across the sky, revealing billions of stars. "+
-                    "Constellations tell ancient stories, while planets like Mars and Saturn captivate with their distinct features."+ 
-                    "The Moon, with its phases and influence on tides, adds to the intrigue."+ 
-                    "Meteor showers bring bursts of light, making stargazing a magical experience.");
-        article1.setVisualitzacions(4500);
-        //article1.setPrivacitat(true);
-        article1.setData("2024-11-14");
-        article1.setImatge("https://www.caracteristicass.de/wp-content/uploads/2023/02/imagenes-artisticas.jpg");
-        article1.setAutor("Autor4");
-        List<Topic> topic = Arrays.asList(Topic.CSS,Topic.JavaScript);
-        article1.setTopics(topic);
-        return article1;
+         Response response = webTarget.path(id.toString())
+                .request(MediaType.APPLICATION_JSON)
+                .get();
+        if (response.getStatus() == 200) {
+            return response.readEntity(ArticleDTO.class);
+        }
+        return null;
     }
     
 }
