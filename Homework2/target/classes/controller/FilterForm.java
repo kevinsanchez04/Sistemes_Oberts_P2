332a3cbec3af5254c6529b8b2576da3b8c735194
Iulian Sebastian Oprea <iuliansebastian.oprea@estudiants.urv.cat>
2024-12-15 18:02:54 +0100

    Better Version Delete Imports

---
+++ Homework2/target/classes/controller/FilterForm.java
@@ -0,0 +1,39 @@
+package deim.urv.cat.homework2.controller;
+
+import java.util.List;
+
+import deim.urv.cat.homework2.model.Topic;
+import jakarta.inject.Named;
+import jakarta.mvc.binding.MvcBinding;
+import jakarta.ws.rs.QueryParam;
+
+@Named("filterForm")
+public class FilterForm {
+    
+    private static final long serialVersionUID = 1L;
+
+    @QueryParam("topics")
+    @MvcBinding
+    private List<Topic> topics;
+    
+    // JSR 303 validation
+    @QueryParam("author")
+    @MvcBinding
+    private String author;
+
+    public List<Topic> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(List<Topic> topics) {
+        this.topics = topics;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
