634b0d64c36df118e4b1ba5942c3b4d9a0c3a589
Iulian <93289638+IulianSebastian@users.noreply.github.com>
2024-12-16 17:13:58 +0100

    Post Article without security

--- Homework2/src/main/java/deim/urv/cat/homework2/controller/ArticleController.java
+++ Homework2/src/main/java/deim/urv/cat/homework2/controller/ArticleController.java
@@ -1,22 +1,32 @@
 package deim.urv.cat.homework2.controller;
 
+import deim.urv.cat.homework2.model.AlertMessage;
+import deim.urv.cat.homework2.model.ArticleDTO;
 import deim.urv.cat.homework2.service.ArticleService;
+import deim.urv.cat.homework2.UnauthorizedExp;
 import jakarta.inject.Inject;
 import jakarta.mvc.Controller;
 import jakarta.mvc.Models;
 import jakarta.mvc.UriRef;
+import jakarta.mvc.binding.BindingResult;
+import jakarta.mvc.binding.ParamError;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.BeanParam;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Controller
 @Path("Article")
 public class ArticleController {
     
+    @Inject BindingResult bindingResult;
     @Inject ArticleService service;
     @Inject Models models;
+    @Inject Logger log;
     
     @GET
     public String showAll() {
@@ -27,8 +37,19 @@ public class ArticleController {
     @GET
     @Path("/{id}")
     public String miMetodo(@PathParam("id") Long id) {
-        models.put("article",service.findId(id));
-        return "article.jsp";
+        try{
+            ArticleDTO art = service.findId(id);
+            if(art == null){
+                models.put("message", "You Have To Be Logged");
+                models.put("articles",service.findAll(null));
+                return "articles.jsp";
+            }
+            models.put("article",art);
+            return "article.jsp";            
+        }catch(NullPointerException e){
+            models.put("message", "Bad Request");
+            return "articles.jsp";
+        }
     }
     
     @GET
@@ -39,4 +60,28 @@ public class ArticleController {
         return "articles.jsp"; // Injects CRSF token
     }
     
+    @GET
+    @Path("/addArticle")
+    @UriRef("addArticle")
+    public String addArticle() {
+        return "articleform.jsp"; // Injects CRSF token
+    }
+    
+    @POST
+    @UriRef("/insert")
+    public String filter(@Valid @BeanParam ArticleForm article) throws UnauthorizedExp {
+        if (bindingResult.isFailed()) {
+            AlertMessage alert = AlertMessage.danger("Validation failed!");
+            bindingResult.getAllErrors()
+                    .stream()
+                    .forEach((ParamError t) -> {
+                        alert.addError(t.getParamName(), "", t.getMessage());
+                    });
+            log.log(Level.WARNING, "Data binding for signupFormController failed.");
+            models.put("errors", alert);
+            return "articleform.jsp";
+        }
+        models.put("articles",service.findAll(null));
+        return "articles.jsp";
+    }
 }
